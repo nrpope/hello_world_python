dc32e04057e27a86b1188a84af634614698d15fc
Nick Pope <psydpope@gmail.com>
2020-08-21 15:50:01 -0700

    small updates

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,6 @@
-# print("Hello World")
-# temperature = "14"
-# print(temperature + " degrees")
+print("Hello World")
+temperature = "14"
+print(temperature + " degrees")
 city_name = "Vancouver"
 province_name = "British Columbia"
 country_name = "Canada"
@@ -8,3 +8,10 @@ print(city_name + ", " + province_name + ", " + country_name + ".")
 
 number_of_application = 5 + 1
 print(number_of_application)
+
+percent = 0.5 * 100
+print(percent)
+
+number_of_steps = 70
+print("You're on step:")
+print(number_of_steps + 1)
