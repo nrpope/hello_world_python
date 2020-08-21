4931278bf9db138e9c0fee514ed1fb1109b87d6d
Nick Pope <psydpope@gmail.com>
2020-08-21 15:42:33 -0700

    first commit

---
+++ Hello World.py
@@ -0,0 +1,10 @@
+# print("Hello World")
+# temperature = "14"
+# print(temperature + " degrees")
+city_name = "Vancouver"
+province_name = "British Columbia"
+country_name = "Canada"
+print(city_name + ", " + province_name + ", " + country_name + ".")
+
+number_of_application = 5 + 1
+print(number_of_application)
