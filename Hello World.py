a231cec0e3f2f251c00e46cdbf47e182e63cad5b
Nick Pope <psydpope@gmail.com>
2020-08-22 15:09:11 -0700

    more variables

--- Hello World.py
+++ Hello World.py
@@ -15,3 +15,29 @@ print(percent)
 number_of_steps = 70
 print("You're on step:")
 print(number_of_steps + 1)
+
+temperature2 = "1" + "8"
+print(temperature2)
+# this will print 18, not 9 because they are both string values
+
+
+player_1 = "Sam"
+player_2 = "Alex"
+current_round = 1
+
+print("Game on!")
+print(f"player 1: {player_1}")
+print(f"player 2: {player_2}")
+print("------------")
+
+print(f"Round {current_round}!")
+player_1_score = 10
+player_2_score = 13
+print(f"{player_2} wins with {player_2_score} to {player_1_score}")
+print("------------")
+
+
+powered_on = True
+powered_off = False
+correct = True
+print(correct)
